7f4c975990a2ec4670844632c4a8be8ea29c0246
student <student@ip-172-31-49-20.us-west-2.compute.internal>
2021-08-07 14:28:40 -0700

    WIP

---
+++ src/_01_nasty_surprise/nastySurprise.java
@@ -0,0 +1,17 @@
+package _01_nasty_surprise;
+
+public class nastySurprise {
+	private void showPictureFromTheInternet(String imageUrl) {
+	    try {
+	        URL url = new URL(imageUrl);
+	        Icon icon = new ImageIcon(url);
+	        JLabel imageLabel = new JLabel(icon);
+	        JFrame frame = new JFrame();
+	        frame.add(imageLabel);
+	        frame.setVisible(true);
+	        frame.pack();
+	    } catch (MalformedURLException e) {
+	        e.printStackTrace();
+	    }
+	}
+}
---
+++ src/_01_nasty_surprise/nastySurpriseRunner.java
@@ -0,0 +1,7 @@
+package _01_nasty_surprise;
+
+public class nastySurpriseRunner {
+	public static void main(String[] args) {
+	
+	}
+}
