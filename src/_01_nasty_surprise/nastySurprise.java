ecb328136366c70f54410c49d7fbf289278c2ce2
student <student@ip-172-31-48-27.us-west-2.compute.internal>
2021-08-21 14:29:55 -0700

    WIP

--- src/_01_nasty_surprise/nastySurprise.java
+++ src/_01_nasty_surprise/nastySurprise.java
@@ -1,6 +1,35 @@
 package _01_nasty_surprise;
 
-public class nastySurprise {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class nastySurprise implements ActionListener {
+	JFrame frame = new JFrame();
+	JButton trick = new JButton();
+	JButton treat = new JButton();
+	JPanel panel = new JPanel();
+	
+	void setup() {
+		trick.setText("Trick");
+		treat.setText("Treat");
+		panel.add(trick);
+		panel.add(treat);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.pack();
+		
+		trick.addActionListener(this);
+		treat.addActionListener(this);
+	}
 	private void showPictureFromTheInternet(String imageUrl) {
 	    try {
 	        URL url = new URL(imageUrl);
@@ -14,4 +43,15 @@ public class nastySurprise {
 	        e.printStackTrace();
 	    }
 	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == trick) {
+			showPictureFromTheInternet("https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg");
+			System.out.println("trick");
+		}
+		if (arg0.getSource() == treat) {
+			System.out.println("treat");
+		}
+	}
+		
 }
--- src/_01_nasty_surprise/nastySurpriseRunner.java
+++ src/_01_nasty_surprise/nastySurpriseRunner.java
@@ -1,7 +1,12 @@
 package _01_nasty_surprise;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class nastySurpriseRunner {
 	public static void main(String[] args) {
-	
+		nastySurprise surprise = new nastySurprise();
+		surprise.setup();
 	}
+	
 }
